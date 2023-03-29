642d33750bf929fa0ebeffae370fefbe208942c2
Jens Kutilek <webmail@kutilek.de>
2023-03-29 12:44:23 +0200

    Improve hashPointPen

--- Stubs/fontTools/pens/hashPointPen.pyi
+++ Stubs/fontTools/pens/hashPointPen.pyi
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from defcon.objects.font import Font
 from fontTools.pens.pointPen import AbstractPointPen
 from typing import Any, Dict
 
@@ -8,7 +9,7 @@ class HashPointPen(AbstractPointPen):
     def __init__(
         self,
         glyphWidth: int = 0,
-        glyphSet: Dict[str, Any] = None,
+        glyphSet: Dict[str, Any] | Font | None = None,
     ): ...
     def drawPoints(self) -> None: ...
     @property
